d5b5b3dff7d1782729f537fd729b394fc2a9125d
pawan <pawanm992002@gmail.com>
2023-05-18 02:29:13 +0530

    medicare backend

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const express = require("express");
-
+const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 8000;
 
+app.use(cors());
 app.use(express.json());
 
 const auth = require("./router/auth");
